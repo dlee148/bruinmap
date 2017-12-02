905c0c7d2150cea10b7be0a7decade7262b0d8c2
Daniel Lee <danieldavidlee@hotmail.com>
2017-12-01 21:49:07 -0800

    added data filters and handlers in the backend

--- js/track.js
+++ js/track.js
@@ -13,25 +13,31 @@ function uploadCoords(position) {
   });
 }
 
-function callPeriodicFunctions() {
-  getLocation();
-  retrievePoints();
-}
-
 function getLocation() {
   navigator.geolocation.getCurrentPosition(uploadCoords);
 }
 
 function retrievePoints() {
-  // TODO: Add get params to filter points
-  $.get("php/retrieve-points.php", null, function(data, status, xhr) {
+  var parameters = {
+    timeFrameStart: $('#timeFrameStart').val(),
+    timeFrameEnd:   $('#timeFrameEnd').val(),
+    dateFrameStart: $('#dateFrameStart').val(),
+    dateFrameEnd:   $('#dateFrameEnd').val(),
+  };
+
+  var animate = $('#animate').is(':checked');
+
+  $.get("php/retrieve-points.php", parameters, function(data, status, xhr) {
     console.log("Retrieve - " + String(xhr.status));
+    $('.loading').hide();
   });
 }
 
 $(document).ready(function() {
+  retrievePoints();
+
   if (navigator.geolocation) {
-    setInterval(callPeriodicFunctions, INTERVAL);
+    // setInterval(getLocation, INTERVAL);
   }
   else {
     var warning = "<div class=\"alert alert-warning\"><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Warning!</strong> Your browswer does not support geolocation, and will therefore not upload location data.</div>";
--- php/retrieve-points.php
+++ php/retrieve-points.php
@@ -11,8 +11,38 @@ if (mysqli_connect_errno()) {
   return;
 }
 
-// TODO: Add and handle GET params to filter data
+// Form query
+$count = 0;
 $query = "SELECT * FROM $table";
+
+if (!empty($_GET['dateFrameStart'])) {
+  $query .= ' WHERE time_stamp >= "' . mysqli_real_escape_string($link, $_GET['dateFrameStart']) . '"';
+
+  $count++;
+}
+if (!empty($_GET['dateFrameEnd'])) {
+  $query .= ($count == 0 ? ' WHERE' : '');
+  $query .= ($count > 0 ? ' AND' : '');
+  $query .= ' time_stamp <= "' . mysqli_real_escape_string($link, $_GET['dateFrameEnd']) . '"';
+
+  $count++;
+}
+if (!empty($_GET['timeFrameStart'])) {
+  $query .= ($count == 0 ? ' WHERE' : '');
+  $query .= ($count > 0 ? ' AND' : '');
+  $query .= ' TIME(time_stamp) >= "' . mysqli_real_escape_string($link, $_GET['timeFrameStart']) . ':00"';
+
+  $count++;
+}
+if (!empty($_GET['timeFrameEnd'])) {
+  $query .= ($count == 0 ? ' WHERE' : '');
+  $query .= ($count > 0 ? ' AND' : '');
+  $query .= ' TIME(time_stamp) <= "' . mysqli_real_escape_string($link, $_GET['timeFrameEnd']). ':59"';
+}
+
+error_log($query);
+
+// Store query result in array
 $rows = array();
 
 if ($result = mysqli_query($link, $query)) {
